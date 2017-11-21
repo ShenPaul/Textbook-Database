53c16d7da1cd1d58a144b79101f5da8c8251ccc9
pieebrahim <pieebrahim@gmail.com>
2017-11-20 22:10:11 -0500

    Add files via upload

--- src/SpreadsheetModel.java
+++ src/SpreadsheetModel.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
@@ -5,29 +6,36 @@ import javax.swing.table.AbstractTableModel;
 class SpreadsheetModel extends AbstractTableModel {
    private String[] columnNames = {"Textbook Number", "Student Number", "Last Name", "First Name", "Teacher", "Course Code", "Date", "Returned"};
 //   private  ArrayList <DataItem> data = new ArrayList <DataItem>();
-   private DataLinkedList list;
+   private DataLinkedList list = new DataLinkedList();
    
 //    private Object[][] data = {
-//    	    {"Kathy", "Smith",
-//    	        "Snowboarding", new Integer(5), new Boolean(false)},
-//    	       {"John", "Doe",
-//    	        "Rowing", new Integer(3), new Boolean(true)},
-//    	       {"Sue", "Black",
-//    	        "Knitting", new Integer(2), new Boolean(false)},
-//    	       {"Jane", "White",
-//    	        "Speed reading", new Integer(20), new Boolean(true)},
-//    	       {"Joe", "Brown",
-//    	        "Pool", new Integer(10), new Boolean(false)},
-//    	   };
+//         {"Kathy", "Smith",
+//             "Snowboarding", new Integer(5), new Boolean(false)},
+//            {"John", "Doe",
+//             "Rowing", new Integer(3), new Boolean(true)},
+//            {"Sue", "Black",
+//             "Knitting", new Integer(2), new Boolean(false)},
+//            {"Jane", "White",
+//             "Speed reading", new Integer(20), new Boolean(true)},
+//            {"Joe", "Brown",
+//             "Pool", new Integer(10), new Boolean(false)},
+//        };
 
-   
+   SpreadsheetModel (String path) {
+	try {
+		list.loadData(path);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+   }
    
     public int getColumnCount() {
         return 8;
     }
 
     public int getRowCount() {
-    	return list.size();
+     return list.size();
     }
 
     public String getColumnName(int col) {
@@ -49,7 +57,7 @@ class SpreadsheetModel extends AbstractTableModel {
     public boolean isCellEditable(int row, int col) {
         //Note that the data/cell address is constant,
         //no matter where the cell appears onscreen.
-    	return true;
+     return true;
     }
 
     /*
@@ -58,9 +66,9 @@ class SpreadsheetModel extends AbstractTableModel {
      */
     public void setValueAt(Object value, int row, int col) {
         list.get(row).set(col, value);
-        fireTableCellUpdated(row, col);
+        fireTableCellUpdated(row, col); //check what this does
     }
-//    ...
 }
 
 //https://docs.oracle.com/javase/tutorial/uiswing/components/table.html save data after editing
+//https://docs.oracle.com/javase/7/docs/api/javax/swing/table/AbstractTableModel.html
