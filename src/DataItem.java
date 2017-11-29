ef78707b140e0fe9bf970c59f1bd889411d360e2
pieebrahim <pieebrahim@gmail.com>
2017-11-28 21:01:39 -0500

    Add files via upload

--- src/DataItem.java
+++ src/DataItem.java
@@ -1,5 +1,5 @@
 class DataItem {
-	
+
 	//class variables
 	private DataItem next;
 	private String itemNum;
@@ -12,7 +12,7 @@ class DataItem {
 	private boolean returned;
 
 	//constructors
-	
+
 	public DataItem(String itemNum, String studentNum, String lastName, String firstName, String teacher, String date, String courseCode, boolean returned) {
 		this.itemNum = itemNum;
 		this.studentNum = studentNum;
@@ -24,45 +24,52 @@ class DataItem {
 		this.next = null;
 		this.returned = returned;
 	}
-	
+
 	public DataItem(String itemNum) {
 		this.itemNum = itemNum;
 	}
 
 	//get and set methods
-	
+
 	public Object get(int n) {
-		switch(n) {
-			case 1: return this.itemNum;
-			case 2: return this.studentNum;
-			case 3: return this.lastName;
-			case 4: return this.firstName;
-			case 5: return this.teacher;
-			case 6: return this.date;
-			case 7: return this.courseCode;
-					
+		if(n==0){
+			return (this.itemNum);
+		}else if(n==1) {
+			return (this.studentNum);
+		}else if(n==2) {
+			return (this.lastName);
+		}else if(n==3) {
+			return (this.firstName);
+		}else if(n==4) {
+			return (this.teacher);
+		}else if(n==5) {
+			return (this.date);
+		}else if(n==6) {
+			return (this.courseCode);
+		}else {
+			return (this.returned);
 		}
-		return null;
 	}
 	public void set(int n, Object value) {
-		switch(n) {
-			case 1: this.itemNum = (String)value;
-					break;
-			case 2: this.studentNum = (String)value;
-					break;
-			case 3: this.lastName = (String)value;
-					break;
-			case 4: this.firstName = (String)value;
-					break;
-			case 5: this.teacher = (String)value;
-					break;
-			case 6: this.date = (String)value;
-					break;
-			case 7: this.courseCode = (String)value;
-					break;
+		if(n==0){
+			this.itemNum = (String)value;
+		}else if(n==1) {
+			this.studentNum = (String)value;
+		}else if(n==2) {
+			this.lastName = (String)value;
+		}else if(n==3) {
+			this.firstName = (String)value;
+		}else if(n==4) {
+			this.teacher = (String)value;
+		}else if(n==5) {
+			this.date = (String)value;
+		}else if(n==6) {
+			this.courseCode = (String)value;
+		}else {
+			this.returned = (Boolean)value;
 		}
 	}
-	
+
 	public DataItem getNext(){
 		return (this.next);
 	}
@@ -70,14 +77,14 @@ class DataItem {
 		this.next = next;
 	}
 
-	
+
 	public void setItemNum(String itemNum){
 		this.itemNum = itemNum;
 	}
 	public String getItemNum(){
 		return this.itemNum;
 	}
-	
+
 	public void setFirstName(String firstName){
 		this.firstName = firstName;
 	}
@@ -90,40 +97,40 @@ class DataItem {
 	public String getLastName(){
 		return this.lastName;
 	}
-	
+
 	public void setStudentNum(String studentNum){
 		this.studentNum = studentNum;
 	}
 	public String getStudentNum(){
 		return this.studentNum;
 	}
-	
+
 	public void setDate(String date){
 		this.date = date;
 	}
 	public String getDate(){
 		return this.date;
 	}
-	
+
 	public void setTeacher(String teacher){
 		this.teacher = teacher;
 	}
 	public String getTeacher(){
 		return this.teacher;
 	}
-	
+
 	public void setCourseCode(String courseCode){
 		this.courseCode = courseCode;
 	}
 	public String getCourseCode(){
 		return this.courseCode;
 	}
-	
+
 	public void setReturned(boolean returned){
 		this.returned = returned;
 	}
 	public boolean getReturned(){
 		return this.returned;
 	}
-	
+
 }
